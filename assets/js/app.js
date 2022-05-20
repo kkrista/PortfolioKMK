d93a4c29cb62d168536238862f7d732115e98d4c
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-05-20 18:29:30 +0300

    Changing dark/light color theme works

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,9 +17,28 @@ const removeActiveLinkClass = e => {
         menuToggleButton.classList.remove('active');
     }
 }
-
 document.addEventListener('click', removeActiveLinkClass);
 
 // !TOGGLE THEME AND STORE SELECTION WITHIN LOCAL STORAGE
+
+const themeToggleButton = document.querySelector('.theme-toggle-button');
+const bodyElement = document.body;
+const currentTheme = localStorage.getItem('darkTheme');
+
+if(currentTheme){
+    bodyElement.classList.add('dark-theme');
+};
+
+const toggleTheme = () => {
+    bodyElement.classList.toggle('dark-theme');
+
+    if(bodyElement.classList.contains('dark-theme')){
+        localStorage.setItem('darkTheme', 'active');
+    }else{
+        localStorage.removeItem('darkTheme');
+    }
+};
+
+themeToggleButton.addEventListener('click', toggleTheme);
 
 // !SCROLL REVEAL
