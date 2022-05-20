41de985f9a7cd6378deb90711832ccc48428f252
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-05-20 18:14:36 +0300

    Javascript added for buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,24 @@
 //!TOGGLE NAVIGATION MENU AND ICONS
+const menuToggleButton = document.querySelector('.menu-toggle-button');
+const menuElement = document.querySelector('.menu');
+
+const toggleMenu = () => {
+    menuElement.classList.toggle('active');
+    menuToggleButton.classList.toggle('active');
+};
+
+menuToggleButton.addEventListener('click', toggleMenu);
 
 // !REMOVE ACTIVE CLASS FROM AND ICON ON LINK CLICK
+
+const removeActiveLinkClass = e => {
+    if(e.target.classList.contains('list-link')){
+        menuElement.classList.remove('active');
+        menuToggleButton.classList.remove('active');
+    }
+}
+
+document.addEventListener('click', removeActiveLinkClass);
 
 // !TOGGLE THEME AND STORE SELECTION WITHIN LOCAL STORAGE
 
